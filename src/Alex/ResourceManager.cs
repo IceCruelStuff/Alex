69601848672a1744a3788004fe083cbed92c4bc7
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2022-11-20 09:38:24 -0800

    update

--- src/Alex/ResourceManager.cs
+++ src/Alex/ResourceManager.cs
@@ -419,7 +419,7 @@ namespace Alex
 				{
 					using (ZipArchive zipArchive = new ZipArchive(Storage.OpenFileStream(bedrockPath, FileMode.Open)))
 					{
-						zipArchive.GetEntry("resource_pack").ExtractToFile(di.FullName, true);
+						zipArchive.GetEntry("resource_pack").ExtractToFile(di.FullName);
 					}
 
 					Storage.Delete(bedrockPath);
